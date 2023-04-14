042028fe0632713d6e28785da1b5a32f1f36a53f
Nizeyumukiza <jpnizeyumukiza@gmail.com>
2023-04-14 14:13:00 -0700

    apparently cub already come with cuda toolkit, no need to check for it

--- setup.py
+++ setup.py
@@ -15,23 +15,23 @@ INSTALL_REQUIREMENTS = []
 include_dirs = [osp.join(ROOT_DIR, "svox2", "csrc", "include")]
 
 # From PyTorch3D
-cub_home = os.environ.get("CUB_HOME", None)
-if cub_home is None:
-	prefix = os.environ.get("CONDA_PREFIX", None)
-	if prefix is not None and os.path.isdir(prefix + "/include/cub"):
-		cub_home = prefix + "/include"
+# cub_home = os.environ.get("CUB_HOME", None)
+# if cub_home is None:
+# 	prefix = os.environ.get("CONDA_PREFIX", None)
+# 	if prefix is not None and os.path.isdir(prefix + "/include/cub"):
+# 		cub_home = prefix + "/include"
 
-if cub_home is None:
-	warnings.warn(
-		"The environment variable `CUB_HOME` was not found."
-		"Installation will fail if your system CUDA toolkit version is less than 11."
-		"NVIDIA CUB can be downloaded "
-		"from `https://github.com/NVIDIA/cub/releases`. You can unpack "
-		"it to a location of your choice and set the environment variable "
-		"`CUB_HOME` to the folder containing the `CMakeListst.txt` file."
-	)
-else:
-	include_dirs.append(os.path.realpath(cub_home).replace("\\ ", " "))
+# if cub_home is None:
+# 	warnings.warn(
+# 		"The environment variable `CUB_HOME` was not found."
+# 		"Installation will fail if your system CUDA toolkit version is less than 11."
+# 		"NVIDIA CUB can be downloaded "
+# 		"from `https://github.com/NVIDIA/cub/releases`. You can unpack "
+# 		"it to a location of your choice and set the environment variable "
+# 		"`CUB_HOME` to the folder containing the `CMakeListst.txt` file."
+# 	)
+# else:
+# 	include_dirs.append(os.path.realpath(cub_home).replace("\\ ", " "))
 
 try:
     ext_modules = [
